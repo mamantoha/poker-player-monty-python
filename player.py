50b1d91ee32db7a5b88662842ac5ec65d39510f0
Oleksiy Markovets <oleksiy.markovets@gmail.com>
2016-03-26 11:29:18 +0200

    refactoring

--- player.py
+++ player.py
@@ -3,19 +3,17 @@ import random
 class Player:
     VERSION = "Default Python folding player"
 
-    def search_player(self, name, game_state):
-        for p in game_state['players']:
-            if p['name'] == name:
-                return p
-
     def betRequest(self, game_state):
-        for player in  game_state['players']:
-            if player['name'] == 'Monty Python':
-                print 'found'
-                for card in player['hole_cards']:
-                    if card['rank'] == 'A':
-                        print "all in", player['stack']
-                        return player['stack']
+        comm = game_state['community_cards']
+        my = self.me(game_state)['hole_cards']
+        me = self.me(game_state)
+
+        if self.pair(comm, my):
+            return me['stack']
+            
+        if self.ace(comm, my):
+            return me['stack']
+            
         return random.randrange(100,300,5)
 
     def showdown(self, game_state):
@@ -24,12 +22,14 @@ class Player:
     def me(self, game_state):
         return game_state['players'][game_state['in_action']]
 
+    def ace(self, comm, my):
+        for card in my:
+            if card['rank'] == 'A':
+                return True
 
-    def has_pair(self, game_state):
-        comm_cards = game_state['community_cards']
-        my_cards = self.me(game_state)['hole_cards']
-        for my_card in my_cards:
-            same = [comm_card for comm_card in comm_cards if my_cards['rank'] == comm_card['rank']]
+    def pair(self, comm, my):
+        for card in my:
+            same = [comm_card for comm_card in comm if card['rank'] == comm_card['rank']]
             if len(same) > 0 :
                 return True
         return False
