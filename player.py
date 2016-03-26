5aa4b228daa41a1a1f7521dc1962e5b4263d5661
Paul <comixan@gmail.com>
2016-03-26 10:48:24 +0200

    Update player.py

--- player.py
+++ player.py
@@ -3,8 +3,8 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        print game_state
-        return 300
+        # print game_state
+        return 1000
 
     def showdown(self, game_state):
         pass
