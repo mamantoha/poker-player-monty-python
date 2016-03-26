95e2f08ea48f4449d637b48d62986eee39422e19
Oleksiy Markovets <oleksiy.markovets@gmail.com>
2016-03-26 11:22:13 +0200

    has pair

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ import random
 class Player:
     VERSION = "Default Python folding player"
 
-    def search_player(name, game_state):
+    def search_player(self, name, game_state):
         for p in game_state['players']:
             if p['name'] == name:
                 return p
@@ -20,4 +20,19 @@ class Player:
 
     def showdown(self, game_state):
         pass
+
+    def me(self, game_state):
+        return game_state['players'][game_state['in_action']]
+
+
+    def has_pair(self, game_state):
+        comm_cards = game_state['community_cards']
+        my_cards = self.me(game_state)['hole_cards']
+        for my_card in my_cards:
+            same = [comm_card for comm_card in comm_cards if my_cards['rank'] == comm_card['rank']]
+            if len(same) > 0 :
+                return True
+        return False
+        
+        
 
