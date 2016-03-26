c2e3ca8901e473b3e836dd17d1474bcd138fc114
Oleksiy Markovets <oleksiy.markovets@gmail.com>
2016-03-26 10:49:40 +0200

    wise

--- player.py
+++ player.py
@@ -3,8 +3,10 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        print game_state
-        return 250
+        for player in  game_state['players']:
+            if player['name'] = 'Monty Python':
+                return player['stack']
+        return 1000
 
     def showdown(self, game_state):
         pass
