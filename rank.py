0732440794fdc5f687378d3d1705d2ab627f443d
Oleksiy Markovets <oleksiy.markovets@gmail.com>
2016-03-26 13:04:21 +0200

    safe rank

--- rank.py
+++ rank.py
@@ -2,17 +2,20 @@ import httplib
 import json, urllib
 
 def rank(cards):
-    data = 'cards=' + json.dumps(cards)
-    body = urllib.urlencode({'cards' : json.dumps(cards)})
+    try:
+        data = 'cards=' + json.dumps(cards)
+        body = urllib.urlencode({'cards' : json.dumps(cards)})
 
-    headers = {"Content-type": "application/x-www-form-urlencoded",
-               "Accept": "text/plain"}
+        headers = {"Content-type": "application/x-www-form-urlencoded",
+                   "Accept": "text/plain"}
 
-    conn = httplib.HTTPConnection("rainman.leanpoker.org")
-    conn.request("GET", "/rank", data, headers)
-    r1 = conn.getresponse()
-    res = r1.read()
-    ranking = json.loads(res)
-    conn.close()
-
-    return ranking['rank']
+        conn = httplib.HTTPConnection("rainman.leanpoker.org")
+        conn.request("GET", "/rank", data, headers)
+        r1 = conn.getresponse()
+        res = r1.read()
+        ranking = json.loads(res)
+        conn.close()
+        return ranking['rank']
+    except:
+        pass
+    return 0
