9ec1e5d47e21bf7b2ab0ddc25535f3e3e8785948
Oleksiy Markovets <oleksiy.markovets@gmail.com>
2016-03-26 11:00:45 +0200

    try

--- player.py
+++ player.py
@@ -4,12 +4,14 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-#        for player in  game_state['players']:
-#            if player['name'] == 'Monty Python':
-#                for card in player['hole_cards']:
-#                    if card['rank'] == 'A':
-#                        return player['stack']
-        return random.randrange(100,500,5)
+        try:
+            for player in  game_state['players']:
+                if player['name'] == 'Monty Python':
+                    for card in player['hole_cards']:
+                        if card['rank'] == 'A':
+                            return player['stack']
+        except:
+            return random.randrange(100,500,5)
 
     def showdown(self, game_state):
         pass
