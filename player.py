fe4654c4f07e8a9e8ea20b0db521ea16c510f58a
Paul <comixan@gmail.com>
2016-03-26 11:14:14 +0200

    Update player.py

--- player.py
+++ player.py
@@ -9,22 +9,8 @@ class Player:
                 return p
 
     def betRequest(self, game_state):
-        try:
-            me = search_player('Monty Python', game_state)
-            print me
-            print "before"
-            for player in  game_state['players']:
-                if player['name'] == 'Monty Python':
-                    print 'found'
-                    for card in player['hole_cards']:
-                        if card['rank'] == 'A':
-                            print "all in", player['stack']
-                            #return player['stack']
-                            return random.randrange(100,300,5)
-            print "after"
-        except:
-            print "Fuck"
-            return random.randrange(100,300,5)
+
+            return 270
 
     def showdown(self, game_state):
         pass
