7169db24ac763444e7fc5b5c21200288e4687554
Paul <comixan@gmail.com>
2016-03-26 12:42:55 +0200

    Update player.py

--- player.py
+++ player.py
@@ -6,10 +6,21 @@ class Player:
     VERSION = "We are not afraid of the rabbit"
 
     def betRequest(self, game_state):
+        
+        list_of_combinations = [('A', 'A', 'p'), ('K', 'K', 'p'), ('3', '3', 'p'), ('4', '4', 'p'), ('5', '5', 'p'),
+        ('6', '6', 'p'),('7', '7', 'p'),('8', '8', 'p'),('9', '9', 'p'),('10', '10', 'p'),('J', 'J', 'p'),('Q', 'Q', 'p')]
+    
         comm = game_state['community_cards']
         my = self.me(game_state)['hole_cards']
         me = self.me(game_state)
-
+        
+        
+        if is_preflop(game_state):
+            for i in list_of_combinations:
+                a, b, c = i 
+                if hole_cards[0]['rank'] == a and hole_cards[1]['rank'] == b:
+                    for player in  game_state['players']:
+                        return player['stack']
         if self.pair(comm, my):
             return self.more(game_state, me)
         elif self.ace(comm, my):
