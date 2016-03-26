aec93af2800554c140b2a4b2ffef3195c2a8a7e5
Oleksiy Markovets <oleksiy.markovets@gmail.com>
2016-03-26 10:53:58 +0200

    more

--- player.py
+++ player.py
@@ -4,9 +4,11 @@ class Player:
 
     def betRequest(self, game_state):
         for player in  game_state['players']:
-            if player['name'] = 'Monty Python':
-                return player['stack']
-        return 1000
+            if player['name'] == 'Monty Python':
+                for card in player['hole_cards']:
+                    if card['rank'] == 'A':
+                        return player['stack']
+        return 0
 
     def showdown(self, game_state):
         pass
