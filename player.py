0eb303248e5103b525a8e118ce4cccd282768f32
Oleksiy Markovets <oleksiy.markovets@gmail.com>
2016-03-26 10:45:22 +0200

    added print

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        print game_state
         return 250
 
     def showdown(self, game_state):
